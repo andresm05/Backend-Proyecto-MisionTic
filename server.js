025af4224d1d058ef2d30be9570bf55c6d362756
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-23 12:22:11 -0500

    puerto 5000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import autorizacionEstadoUsuario from './middleware/estadoUsuario.js';
 
 
 dotenv.config({ path: "./.env" });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = Express();
 
 app.use(Express.json());
