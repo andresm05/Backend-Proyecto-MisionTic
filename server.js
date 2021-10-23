60ad80445349c78a1b6a33975473235379ee6dd3
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-23 12:05:30 -0500

    cambiar puerto

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import autorizacionEstadoUsuario from './middleware/estadoUsuario.js';
 
 
 dotenv.config({ path: "./.env" });
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 const app = Express();
 
 app.use(Express.json());
